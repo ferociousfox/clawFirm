25b48e6b6339d6368c11a60956f7eace13a79b90
Ginger Lee Kretschmer, Luke Bertram and Shane Ryan <ginger+luke+shane@>
2018-02-08 09:50:37 -0800

    set up routing in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
+import Home from '../containers/Home';
+import AboutUs from '../containers/AboutUs.jsx';
+import OurTeam from '../containers/OurTeam.jsx';
 
 function App(){
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={Default} />
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={AboutUs} />
+        <Route path='/team' component={OurTeam} />
         <Route component={Error404} />
       </Switch>
     </div>
   );
 }
 
-//delete the following component definition before use
-function Default(props){
-  return(
-    <h1>Default Component/App/Router Works!</h1>
-  );
-}
 
 export default App;
